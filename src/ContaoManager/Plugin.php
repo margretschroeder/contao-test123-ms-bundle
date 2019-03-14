f2d7c0f81ae2888c892e61e26d6379abbdd56f14
margretschroeder <margret_schroeder@freenet.de>
2019-03-14 18:55:04 +0100

    Initial commit

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -7,18 +7,31 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Margretschroeder\ContaoTest123MsBundle\ContaoTest123Bundle;
+use Margretschroeder\ContaoTest123MsBundle\ContaoTest123MsBundle;
 
 class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
-     */
+    
     public function getBundles(ParserInterface $parser)
+    {
+    
+	    return [
+            BundleConfig::create(ContaoTest123MsBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+	
+	 */
+	public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(ContaoTest123MsBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
+	
+	
+	
 }
